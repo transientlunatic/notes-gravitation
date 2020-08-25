9407eda79740a64ee26163e1f96a4ba20b7a67ef
Daniel Williams <mail@daniel-williams.co.uk>
2020-08-25 14:58:48 +0100

    Updated theme to kentigern

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-import sphinx_daniel_theme
+import kentigern
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -32,7 +32,9 @@ release = 'v0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgconverter', 'sphinxcontrib.bibtex', 'sphinxcontrib.tikz']
+extensions = ['sphinx.ext.imgconverter',
+              'sphinxcontrib.bibtex',
+              'sphinxcontrib.tikz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,8 +51,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 
-html_theme = 'daniel'
-html_theme_path = sphinx_daniel_theme.get_html_theme_path()
+html_theme = 'kentigern'
+html_theme_path = kentigern.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
